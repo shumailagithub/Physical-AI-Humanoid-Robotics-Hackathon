f98707ae83bc0c5346fb0dff8f79d7a9a5503989
Shumaila <shumailagulfam3@gmail.com>
2025-12-19 12:52:58 +0500

    url changes

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -4,7 +4,7 @@ import useIsBrowser from '@docusaurus/useIsBrowser';
 import { JSX } from 'react/jsx-runtime';
 
 // API Configuration
-const API_URL = 'web-production-0039.up.railway.app';
+const API_URL = 'https://web-production-0039.up.railway.app';
 const API_KEY = 'shumaila1234'
 // Helper function for mobile-friendly fetch with timeout
 async function fetchWithTimeout(
